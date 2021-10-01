7bef377d3980c4f1328877f216b0b323a248bacb
Altaha <altahabinsyafriadi@gmail.com>
2021-10-01 15:17:06 +0700

    commit kesekian

--- app/src/main/java/com/example/lab6pertemuan1/MainActivity.java
+++ app/src/main/java/com/example/lab6pertemuan1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.lab6pertemuan1;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -21,11 +25,18 @@ public class MainActivity extends AppCompatActivity {
         //memasukkan gambar dari url ke komponen imageview
         Picasso.with(this).load(urldariinternet).into(kotakGambar);
 
+        Button tombol=findViewById(R.id.tombolpertama);
 
-        //variabel primitive: string, int, boolean
-        //variabel reference: object
+        tombol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pindahHalaman();
+            }
+        });
+    }
 
-
-
+    public void pindahHalaman(){
+        Intent i = new Intent(this, ActivityKedua.class);
+        startActivity(i);
     }
 }
